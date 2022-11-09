a289c8f64ca328ebb9d089875e69c8865782506f
unknown <nathan.manaranche@gmail.com>
2022-11-09 10:13:51 +0100

    Resolution (2e version) mail-sender.test.js

--- js/tests/mail-sender.test.js
+++ js/tests/mail-sender.test.js
@@ -1,41 +1,65 @@
 const sender = require('../mail-sender')
 const test = require('tape')
 
+class ResponseTest {
+  constructor(data, code)
+  {
+    this.code = code
+    this.data = data
+  }
+}
+
 class HttpClientTest {
-  post(baseUrl, request) {
+  post(baseUrl, request)
+  {
+    this.checkRequestConsistency(request)
 
-    if (!request.name
-      || !request.email
-      || !request.subject
-      || !request.message) 
-      return {code: 400};
+    if(request instanceof ResponseTest)
+    {
+      throw { message: "Request is instanceof ResponseTest"}
+    }
 
-    this.request = request;
+    return new ResponseTest({}, 503)
+  }
 
-    return {code: 503};
+  checkRequestConsistency(request)
+  {
+    const regExp = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)
+
+    if(!regExp.test(request.email))
+    {
+      throw { message: "email is not standard"}
+    }
   }
 }
 
 test('send v1', (t) => {
+  const httpClient = new HttpClientTest()
+  const mailSender = new sender.MailSender(httpClient)
+  const user = {name: "name", email: "mail@mail.com"}
 
-  // TODO: write a test that fails due to the bug in
-  const user = {name: "Jean", email: "jean@test.com"};
-  const mailSender = new sender.MailSender(new HttpClientTest());
-  mailSender.sendV1(user, "Hello world");
-
-  t.equal(mailSender.httpClient.request.name, user.name);
+  try {
+    mailSender.sendV1(user, "test")
+  }
+  catch(err) {
+    t.error(err)
+  }
 
   t.end()
 })
 
 test('send v2', (t) => {
+  const httpClient = new HttpClientTest()
+  const mailSender = new sender.MailSender(httpClient)
+  const user = {name: "test", email: "test@test"}
 
-  // TODO: wrte a test that fails due to the bug in
-  const user = {name: "Jean", email: "jean@test.com"};
-  const mailSender = new sender.MailSender(new HttpClientTest());
-  const response = mailSender.sendV2(user, "Hello world");
-
-  t.equal(response.code, 503);
+  try {
+    const response = mailSender.sendV2(user, "test")
+  }
+  catch (error)
+  {
+    t.error(error.message)
+  }
 
   t.end()
 })
