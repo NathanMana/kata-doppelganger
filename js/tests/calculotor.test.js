5b1835a3cc1963d2c1ec40e9046b2d11399783cf
unknown <nathan.manaranche@gmail.com>
2022-11-09 09:11:54 +0100

    Suppression classe AuthorizeTest inutilisée

--- js/tests/calculotor.test.js
+++ js/tests/calculotor.test.js
@@ -1,12 +1,6 @@
 const test = require("tape");
 const Calculator = require("../calculator");
 
-class AuthorizerTest {
-  authorize() {
-    return true;
-  }
-}
-
 test('should throw when not authorized', (t) => {
   const calculator = new Calculator({
     authorize: () => false,
